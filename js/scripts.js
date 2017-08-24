19872bd23ffec3c5c223ecce4c4a150b35193b00
Mark Helt and senya webster <mark@>
2017-08-24 14:04:32 -0700

    test

--- js/scripts.js
+++ js/scripts.js
@@ -72,107 +72,122 @@ function checkWinner(Player) {
 $(document).ready(function(){
   $("#square1").click(function(event){
     event.preventDefault();
-    newBoard.grid[0][0].symbol = symbol
-    newBoard.grid[0][0].value = 1
-    player1.updateScore();
-    player2.updateScore();
-    $("#square1").html("<h1>" + symbol + "</h1>")
-    checkWinner(player1);
-    checkWinner(player2);
-    switchPlayer();
+    if (!newBoard.grid[0][0].isOccupied()) {
+      newBoard.grid[0][0].symbol = symbol
+      newBoard.grid[0][0].value = 1
+      player1.updateScore();
+      player2.updateScore();
+      $("#square1").html("<h1>" + symbol + "</h1>")
+      checkWinner(player1);
+      checkWinner(player2);
+      switchPlayer();
+    }
   });
   $("#square2").click(function(event){
     event.preventDefault();
-    newBoard.grid[0][1].symbol = symbol
-    newBoard.grid[0][1].value = 2
-    player1.updateScore();
-    player2.updateScore();
-    $("#square2").html("<h1>" + symbol + "</h1>")
-    checkWinner(player1);
-    checkWinner(player2);
-    switchPlayer();
+    if (!newBoard.grid[0][1].isOccupied()) {
+      newBoard.grid[0][1].symbol = symbol
+      newBoard.grid[0][1].value = 2
+      player1.updateScore();
+      player2.updateScore();
+      $("#square2").html("<h1>" + symbol + "</h1>")
+      checkWinner(player1);
+      checkWinner(player2);
+      switchPlayer();
+    }
   });
   $("#square3").click(function(event){
     event.preventDefault();
-    newBoard.grid[0][2].symbol = symbol
-    newBoard.grid[0][2].value = 4
-    player1.updateScore();
-    player2.updateScore();
-    $("#square3").html("<h1>" + symbol + "</h1>")
-    checkWinner(player1);
-    checkWinner(player2);
-    switchPlayer();
+    if (!newBoard.grid[0][2].isOccupied()) {
+      newBoard.grid[0][2].symbol = symbol
+      newBoard.grid[0][2].value = 4
+      player1.updateScore();
+      player2.updateScore();
+      $("#square3").html("<h1>" + symbol + "</h1>")
+      checkWinner(player1);
+      checkWinner(player2);
+      switchPlayer();
+    }
   });
   $("#square4").click(function(event){
     event.preventDefault();
-    newBoard.grid[1][0].symbol = symbol
-    newBoard.grid[1][0].value = 8
-    player1.updateScore();
-    player2.updateScore();
-    $("#square4").html("<h1>" + symbol + "</h1>")
-    checkWinner(player1);
-    checkWinner(player2);
-    switchPlayer();
+    if (!newBoard.grid[1][0].isOccupied()) {
+      newBoard.grid[1][0].symbol = symbol
+      newBoard.grid[1][0].value = 8
+      player1.updateScore();
+      player2.updateScore();
+      $("#square4").html("<h1>" + symbol + "</h1>")
+      checkWinner(player1);
+      checkWinner(player2);
+      switchPlayer();
+    }
   });
   $("#square5").click(function(event){
     event.preventDefault();
-    newBoard.grid[1][1].symbol = symbol
-    newBoard.grid[1][1].value = 16
-    player1.updateScore();
-    player2.updateScore();
-    $("#square5").html("<h1>" + symbol + "</h1>")
-    checkWinner(player1);
-    checkWinner(player2);
-    switchPlayer();
+    if (!newBoard.grid[1][1].isOccupied()) {
+      newBoard.grid[1][1].symbol = symbol
+      newBoard.grid[1][1].value = 16
+      player1.updateScore();
+      player2.updateScore();
+      $("#square5").html("<h1>" + symbol + "</h1>")
+      checkWinner(player1);
+      checkWinner(player2);
+      switchPlayer();
+    }
   });
   $("#square6").click(function(event){
     event.preventDefault();
-    newBoard.grid[1][2].symbol = symbol
-    newBoard.grid[1][2].value = 32
-    player1.updateScore();
-    player2.updateScore();
-    $("#square6").html("<h1>" + symbol + "</h1>")
-    checkWinner(player1);
-    checkWinner(player2);
-    switchPlayer();
+    if (!newBoard.grid[1][2].isOccupied()) {
+      newBoard.grid[1][2].symbol = symbol
+      newBoard.grid[1][2].value = 32
+      player1.updateScore();
+      player2.updateScore();
+      $("#square6").html("<h1>" + symbol + "</h1>")
+      checkWinner(player1);
+      checkWinner(player2);
+      switchPlayer();
+    }
   });
   $("#square7").click(function(event){
     event.preventDefault();
-    newBoard.grid[2][0].symbol = symbol
-    newBoard.grid[2][0].value = 64
-    player1.updateScore();
-    player2.updateScore();
-    $("#square7").html("<h1>" + symbol + "</h1>")
-    checkWinner(player1);
-    checkWinner(player2);
-    switchPlayer();
+    if (!newBoard.grid[2][0].isOccupied()) {
+      newBoard.grid[2][0].symbol = symbol
+      newBoard.grid[2][0].value = 64
+      player1.updateScore();
+      player2.updateScore();
+      $("#square7").html("<h1>" + symbol + "</h1>")
+      checkWinner(player1);
+      checkWinner(player2);
+      switchPlayer();
+    }
   });
   $("#square8").click(function(event){
     event.preventDefault();
-    newBoard.grid[2][1].symbol = symbol
-    newBoard.grid[2][1].value = 128
-    player1.updateScore();
-    player2.updateScore();
-    $("#square8").html("<h1>" + symbol + "</h1>")
-    checkWinner(player1);
-    checkWinner(player2);
-    switchPlayer();
+    if (!newBoard.grid[2][1].isOccupied()) {
+      newBoard.grid[2][1].symbol = symbol
+      newBoard.grid[2][1].value = 128
+      player1.updateScore();
+      player2.updateScore();
+      $("#square8").html("<h1>" + symbol + "</h1>")
+      checkWinner(player1);
+      checkWinner(player2);
+      switchPlayer();
+    }
   });
   $("#square9").click(function(event){
     event.preventDefault();
-    newBoard.grid[2][2].symbol = symbol
-    newBoard.grid[2][2].value = 256
-    player1.updateScore();
-    player2.updateScore();
-    $("#square9").html("<h1>" + symbol + "</h1>")
-    checkWinner(player1);
-    checkWinner(player2);
-    switchPlayer();
-    console.log(newBoard);
-    console.log(player1);
-    console.log(player2);
-
-
-
+    if (!newBoard.grid[2][2].isOccupied()) {
+      newBoard.grid[2][2].symbol = symbol
+      newBoard.grid[2][2].value = 256
+      player1.updateScore();
+      player2.updateScore();
+      $("#square9").html("<h1>" + symbol + "</h1>")
+      checkWinner(player1);
+      checkWinner(player2);
+      switchPlayer();
+      console.log(newBoard);
+      console.log(player1);
+      console.log(player2);
+    }
   });
 });
